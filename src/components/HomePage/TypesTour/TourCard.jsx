302092b5b5cfb2f9c8fc48f67e23d4ab7ace966f
shubham sharma <wifibri2@gmail.com>
2024-05-17 11:00:15 +0530

    lazy commit

--- src/components/HomePage/TypesTour/TourCard.jsx
+++ src/components/HomePage/TypesTour/TourCard.jsx
@@ -3,7 +3,15 @@ import Link from "next/link";
 import React from "react";
 import { FaStar } from "react-icons/fa";
 import { MdOutlineAddShoppingCart } from "react-icons/md";
-const TourCard = ({_id,price,tour_name,destination,description,currency ,images}) => {
+const TourCard = ({
+  _id,
+  price,
+  tour_name,
+  destination,
+  description,
+  currency,
+  images,
+}) => {
   return (
     <div className="min-w-[150px] w-[130px] sm:w-[160px] md:min-w-[170px] md:w-[170px] rounded-xl overflow-hidden relative bg-white shadow-lg">
       <div className="w-full">
@@ -16,12 +24,12 @@ const TourCard = ({_id,price,tour_name,destination,description,currency ,images}
       </div>
       <div className="px-4 sm:px-4 my-2 mb-3 sm:my-4 ">
         <Tooltip content={tour_name}>
-        <h3 className="text-[12px] sm:text-sm font-medium line-clamp-1">
-        {tour_name}
-        </h3>
+          <h3 className="text-[12px] sm:text-sm font-medium line-clamp-1">
+            {tour_name}
+          </h3>
         </Tooltip>
         <h3 className="text-xs font-medium text-richblack-900 mt-1">
-          {currency?.symbol} {" "} {price?.sharing_price}
+          {currency?.symbol} {price?.sharing_price}
         </h3>
         <div className="flex items-center justify-between flex-wrap sm:flex-nowrap mt-0 sm:mt-1">
           {/* <p className="text-[8px] text-richblack-200 flex items-center mt-1">277 Reviews</p> */}
@@ -34,7 +42,7 @@ const TourCard = ({_id,price,tour_name,destination,description,currency ,images}
           </div>
         </div>
         <div className="flex !items-strech gap-1 mt-3">
-        <Link href={`/tour/${_id}`} passHref >
+          <Link href={`/tour/${_id}`}>
             <Button
               size="sm"
               variant="outlined"
@@ -42,10 +50,9 @@ const TourCard = ({_id,price,tour_name,destination,description,currency ,images}
             >
               Book Now
             </Button>
-         
           </Link>
 
-         <button className="bg-none relative z-20 px-1 pl-2 ml-3 border-none before:w-full before:h-full before:bg-none before:hover:bg-richblack-25 before:absolute before:top-0 before:left-[1px] before:py-2 before:rounded-full before:z-0" >
+          <button className="bg-none relative z-20 px-1 pl-2 ml-3 border-none before:w-full before:h-full before:bg-none before:hover:bg-richblack-25 before:absolute before:top-0 before:left-[1px] before:py-2 before:rounded-full before:z-0">
             <MdOutlineAddShoppingCart className="text-lg relative z-30 text-richblack-500"></MdOutlineAddShoppingCart>
           </button>
         </div>
