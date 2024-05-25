5b09ab4e612af1930e9649c21095ba6671ed78d5
shubham sharma <wifibri2@gmail.com>
2024-05-25 13:14:31 +0530

    lazy commit

--- src/components/HomePage/TabsAndPlanTours/TabsSelection.jsx
+++ src/components/HomePage/TabsAndPlanTours/TabsSelection.jsx
@@ -12,15 +12,15 @@ const TabsSelection = ({ setIsOpen, isOpen }) => {
   return (
     <div className="w-full max-w-maxContent mx-auto border-b border-gray-200 py-2 overflow-auto pl-2 md:px-4 flex items-center sticky top-[50px] z-[100] select-none bg-white pr-2">
       <div className="flex justify-start items-center gap-x-2">
-        <Link href={"trips"} className="px-3 py-1 flex justify-center items-center gap-x-2  text-gray-900 text-base font-medium hover:bg-gray-100 rounded-md min-w-fit cursor-pointer">
+        <Link href={"/home/trips"} className="px-3 py-1 flex justify-center items-center gap-x-2  text-gray-900 text-base font-medium hover:bg-gray-100 rounded-md min-w-fit cursor-pointer">
           <FaPlaneDeparture className="text-gray-600"></FaPlaneDeparture>
           <span>Trips</span>
         </Link>
-        <Link href={"hotels"} className="px-3 py-1 flex justify-center items-center gap-x-2  text-gray-900 text-base font-medium hover:bg-gray-100 rounded-md min-w-fit cursor-pointer">
+        <Link href={"/home/hotels"} className="px-3 py-1 flex justify-center items-center gap-x-2  text-gray-900 text-base font-medium hover:bg-gray-100 rounded-md min-w-fit cursor-pointer">
           <FaHotel className="text-gray-600"></FaHotel>
           <span>Hotels</span>
         </Link>
-        <Link href={"packages"} className="px-3 py-1 flex justify-center items-center gap-x-2  text-gray-900 text-base font-medium hover:bg-gray-100 rounded-md min-w-fit cursor-pointer">
+        <Link href={"/home/packages"} className="px-3 py-1 flex justify-center items-center gap-x-2  text-gray-900 text-base font-medium hover:bg-gray-100 rounded-md min-w-fit cursor-pointer">
           <GoPackage className="text-gray-600"></GoPackage>
           <span>Packages</span>
         </Link>
