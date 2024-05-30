afa6bdf09fd63eb82993517118b9f3f12d739916
shubham sharma <wifibri2@gmail.com>
2024-05-30 17:25:19 +0530

    lazy commit

--- src/components/HomePage/TopPackages/index.jsx
+++ src/components/HomePage/TopPackages/index.jsx
@@ -1,17 +1,32 @@
 import React from "react";
+import { FaHeart } from "react-icons/fa";
 
 const TopPackages = () => {
   return (
     <div>
-      <h1 className="text-lg px-6">Top Packages</h1>
+      <h1 className="text-lg px-6 mt-2">Top Packages</h1>
       <div className="w-full max-w-maxContent mx-auto flex justify-start p-4 gap-x-2 overflow-auto hide-scrollbar">
         <div className="min-w-full max-w-[430px] h-[220px] rounded-2xl overflow-hidden relative">
           <img
-            src="/images/banner-2.jpg"
+            src="/images/banner-1.jpg"
             alt=""
-            className="w-full rounded-2xl absolute top-0 left-0 w-full h-full z-0"
+            className="w-full rounded-2xl absolute top-0 left-0 w-full h-full z-0 object-cover"
           />
-          <div className="absolute w-full h-full top-0 left-0 bg-[rgba(0,0,0,0.35)] text-white z-10 rounded-2xl px-4 py-2">
+
+          {/* Top text container */}
+          <div className="absolute w-full top-0 left-0 z-50 text-white px-4 py-3 flex gap-x-2 justify-end bg-gradient-to-b from-[rgba(0,0,0,0.4)] to-transparent">
+          
+            <div>
+              <div className="text-white bg-[rgba(299,299,299,0.2)] backdrop-blur-sm flex gap-x-1 items-center rounded-full px-1 py-1 pr-2">
+                <div className="w-[30px] h-[30px] rounded-full bg-[rgba(299,299,299,0.2)] flex items-center justify-center ">
+                <FaHeart></FaHeart>
+                </div>
+                <span className="text-xs GTE_light">Favourite</span>
+              </div>
+            </div>
+          </div>
+          {/* Bottom text container */}
+          <div className="absolute w-full h-full top-0 left-0 bg-gradient-to-b from-transparent via-transparent to-[rgba(0,0,0,0.7)] text-white z-10 rounded-2xl px-4 py-2">
             <div className="relative top-full -translate-y-full flex justify-between w-full">
               <div>
                 <p className="text-sm GTE_light text-gray-300">River Galego</p>
@@ -20,7 +35,7 @@ const TopPackages = () => {
               <div className="flex gap-x-1 items-center GTE_light">
                 <h1>$599</h1>
                 <p className="text-gray-400 GTE_light">/</p>
-                <p className="text-gray-400 text-xs GTE_light">2 Person</p>
+                <p className="text-gray-400 text-xs GTE_light"> Person</p>
               </div>
             </div>
           </div>
@@ -29,9 +44,23 @@ const TopPackages = () => {
           <img
             src="/images/banner-2.jpg"
             alt=""
-            className="w-full rounded-2xl absolute top-0 left-0 w-full h-full z-0"
+            className="w-full rounded-2xl absolute top-0 left-0 w-full h-full z-0 object-cover"
           />
-          <div className="absolute w-full h-full top-0 left-0 bg-[rgba(0,0,0,0.35)] text-white z-10 rounded-2xl px-4 py-2">
+
+          {/* Top text container */}
+          <div className="absolute w-full top-0 left-0 z-50 text-white px-4 py-3 flex gap-x-2 justify-end bg-gradient-to-b from-[rgba(0,0,0,0.4)] to-transparent">
+          
+            <div>
+              <div className="text-white bg-[rgba(299,299,299,0.2)] backdrop-blur-sm flex gap-x-1 items-center rounded-full px-1 py-1 pr-2">
+                <div className="w-[30px] h-[30px] rounded-full bg-[rgba(299,299,299,0.2)] flex items-center justify-center ">
+                <FaHeart></FaHeart>
+                </div>
+                <span className="text-xs GTE_light">Favourite</span>
+              </div>
+            </div>
+          </div>
+          {/* Bottom text container */}
+          <div className="absolute w-full h-full top-0 left-0 bg-gradient-to-b from-transparent via-transparent to-[rgba(0,0,0,0.7)] text-white z-10 rounded-2xl px-4 py-2">
             <div className="relative top-full -translate-y-full flex justify-between w-full">
               <div>
                 <p className="text-sm GTE_light text-gray-300">River Galego</p>
@@ -40,18 +69,32 @@ const TopPackages = () => {
               <div className="flex gap-x-1 items-center GTE_light">
                 <h1>$599</h1>
                 <p className="text-gray-400 GTE_light">/</p>
-                <p className="text-gray-400 text-xs GTE_light">2 Person</p>
+                <p className="text-gray-400 text-xs GTE_light"> Person</p>
               </div>
             </div>
           </div>
         </div>
         <div className="min-w-full max-w-[430px] h-[220px] rounded-2xl overflow-hidden relative">
           <img
-            src="/images/banner-2.jpg"
+            src="/images/banner-3.jpg"
             alt=""
-            className="w-full rounded-2xl absolute top-0 left-0 w-full h-full z-0"
+            className="w-full rounded-2xl absolute top-0 left-0 w-full h-full z-0 object-cover"
           />
-          <div className="absolute w-full h-full top-0 left-0 bg-[rgba(0,0,0,0.35)] text-white z-10 rounded-2xl px-4 py-2">
+
+          {/* Top text container */}
+          <div className="absolute w-full top-0 left-0 z-50 text-white px-4 py-3 flex gap-x-2 justify-end bg-gradient-to-b from-[rgba(0,0,0,0.4)] to-transparent">
+          
+            <div>
+              <div className="text-white bg-[rgba(299,299,299,0.2)] backdrop-blur-sm flex gap-x-1 items-center rounded-full px-1 py-1 pr-2">
+                <div className="w-[30px] h-[30px] rounded-full bg-[rgba(299,299,299,0.2)] flex items-center justify-center ">
+                <FaHeart></FaHeart>
+                </div>
+                <span className="text-xs GTE_light">Favourite</span>
+              </div>
+            </div>
+          </div>
+          {/* Bottom text container */}
+          <div className="absolute w-full h-full top-0 left-0 bg-gradient-to-b from-transparent via-transparent to-[rgba(0,0,0,0.7)] text-white z-10 rounded-2xl px-4 py-2">
             <div className="relative top-full -translate-y-full flex justify-between w-full">
               <div>
                 <p className="text-sm GTE_light text-gray-300">River Galego</p>
@@ -60,11 +103,12 @@ const TopPackages = () => {
               <div className="flex gap-x-1 items-center GTE_light">
                 <h1>$599</h1>
                 <p className="text-gray-400 GTE_light">/</p>
-                <p className="text-gray-400 text-xs GTE_light">2 Person</p>
+                <p className="text-gray-400 text-xs GTE_light"> Person</p>
               </div>
             </div>
           </div>
         </div>
+   
       </div>
     </div>
   );
--- src/components/HomePage/TopTourWorldWide/index.jsx
+++ src/components/HomePage/TopTourWorldWide/index.jsx
@@ -4,12 +4,12 @@ import TourCard from "../TypesTour/TourCard";
 
 const TopTourWorldWide = () => {
   return (
-    <div className="w-full max-w-maxWidthContent mx-auto px-4">
-      <div className="rounded-3xl bg-gradient-to-b from-orange-50 via-transparent to-transparent py-4">
+    <div className="w-full max-w-maxWidthContent mx-auto">
+      {/* <div className="rounded-3xl bg-gradient-to-b from-orange-50 via-transparent to-transparent py-4"> */}
 
-      <h1 className="text-lg px-4">Top Things to Do Worldwide</h1>
+      <h1 className="text-lg px-6">Top Things to Do Worldwide</h1>
       <div>
-        <div className="flex gap-x-2 py-2 px-4 items-center">
+        <div className="flex gap-x-2 py-2 items-center  px-6">
           <div className="min-w-fit w-fit px-2 py-2 text-[12px] md:text-sm text-black rounded-full cursor-pointer transition-all border flex items-center gap-x-2">
             <img src="https://cdn.pixabay.com/photo/2012/04/10/23/03/india-26828_640.png" alt="" className="w-[20px] h-[20px] rounded-xl object-cover" />
             <span className="GTE_light text-xs sm:text-sm text-black rounded-full">
@@ -36,14 +36,14 @@ const TopTourWorldWide = () => {
           </div>
         </div>
 
-        <div className="py-4 flex gap-x-4 md:gap-x-3 items-center overflow-auto hide-scrollbar px-4">
+        <div className="py-4 flex gap-x-4 md:gap-x-3 items-center overflow-auto hide-scrollbar  px-6">
          <TourCard _id={"dasd"} currency={{symbol:"AED"}} tour_name={"Adu Dhabi Safari"} destination={"Dubai"} images={["/images/banner-1.jpg"]} price={{sharing_price:"390"}} key={"sd"}></TourCard>
          <TourCard _id={"dasd"} currency={{symbol:"AED"}} tour_name={"Adu Dhabi Safari"} destination={"Dubai"} images={["/images/banner-1.jpg"]} price={{sharing_price:"390"}} key={"sd"}></TourCard>
          <TourCard _id={"dasd"} currency={{symbol:"AED"}} tour_name={"Adu Dhabi Safari"} destination={"Dubai"} images={["/images/banner-1.jpg"]} price={{sharing_price:"390"}} key={"sd"}></TourCard>
          <TourCard _id={"dasd"} currency={{symbol:"AED"}} tour_name={"Adu Dhabi Safari"} destination={"Dubai"} images={["/images/banner-1.jpg"]} price={{sharing_price:"390"}} key={"sd"}></TourCard>
         </div>
       </div>
-      </div>
+      {/* </div> */}
     </div>
   );
 };
